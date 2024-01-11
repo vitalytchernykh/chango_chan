1261887056dcc112a55615add12ec74b97561a6c
vitalytchernykh <vitaly.tchernykh@gmail.com>
2024-01-11 13:32:30 +0300

    Create chatBot.py

---
+++ modules/chatBot.py
@@ -0,0 +1,26 @@
+from aiogram import Bot
+from aiohttp import ClientSession
+from aiologger.loggers.json import JsonLogger
+from logging import INFO, DEBUG
+
+
+class ChatBot:
+  ''' chat bot object
+  init default values '''
+
+  tg_bot: Bot
+  tg_bot_logger: JsonLogger
+  hf_session: ClientSession
+  hf_model_name: str
+  HF_API_URL: str
+  hf_headers: dict
+
+  def __init__(self):
+    self.tg_bot_logger = JsonLogger.with_default_handlers(
+        name='app-logger',
+        level=INFO,
+        serializer_kwargs={'ensure_ascii': False},
+    )
+    self.hf_model_name = ''
+    self.HF_API_URL = ''
+    self.hf_headers = {}
